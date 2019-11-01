87fe400ac4e6e5d64f8404154912f4d8377a2e11
yixin.huangrongyou <huangrongyou@yixin.im>
2019-11-01 17:47:29 +0800

    1. 更新

---
+++ algorithm/src/main/java/com/hry/algorithm/leetcode/ZigZagConversion.java
@@ -0,0 +1,25 @@
+package com.hry.algorithm.leetcode;
+
+/**
+ *
+ * 6. ZigZag Conversion
+ *  https://leetcode.com/problems/zigzag-conversion/
+ *
+ * 算法解释：
+ *  https://zhuanlan.zhihu.com/p/30181688
+ *
+ * @author: huangrongyou@yixin.im
+ * @date: 2019/10/31 20:05
+ */
+public class ZigZagConversion {
+
+    public String convert(String s, int numRows) {
+        // 创建二位数组
+        int row  = numRows;
+
+        int partNum = numRows + numRows;
+
+        return null;
+    }
+
+}
